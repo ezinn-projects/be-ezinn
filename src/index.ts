50c54b129e00abaae76391e9b1c6565842ea75d3
QuangDo <do.quang03@gmail.com>
2025-01-05 18:10:43 +0700

    update

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import roomTypeRouter from '~/routes/roomType.routes'
 import usersRouter from '~/routes/users.routes'
 import databaseService from '~/services/database.services'
 import serverService from '~/services/server.services'
-import PriceRouter from '~/routes/Price.routes'
+import PriceRouter from './routes/price.routes'
 
 databaseService.connect()
 serverService.start()
